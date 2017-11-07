f7823e59e37a49091a8b37e80c3a6ffbc66cbc8f
manuelbg112795 <32806153+manuelbg112795@users.noreply.github.com>
2017-11-07 00:03:11 -0600

    Manuel 07/11/2017 12:02

--- app/src/main/java/com/example/adrian/appe/FormularioGeneral2.java
+++ app/src/main/java/com/example/adrian/appe/FormularioGeneral2.java
@@ -20,9 +20,12 @@ public class FormularioGeneral2 extends AppCompatActivity {
     int ID;
     String Actividad;
 
+    //COMPLETADO sección de botónes
     Button btnRegresar;
     Button btnEnviarRegistro;
+    Button btnIngresa;
 
+    //COMPLETADO sección de Layout
     LinearLayout lytSeccionWeb;
     LinearLayout lytSeccionLider;
     LinearLayout lytSeccionCertificados;
@@ -36,6 +39,11 @@ public class FormularioGeneral2 extends AppCompatActivity {
     TextView txtTitulo;
     TextView txtDescripcion;
 
+    // COMPLETADO Sección de Checkbox
+    CheckBox chkFacebook;
+    CheckBox chkTwitter;
+    CheckBox chkInstagram;
+    CheckBox chkSnapchat;
     CheckBox chkProteccion;
     CheckBox chkSismologico;
     CheckBox chkMeteorologico;
@@ -49,6 +57,50 @@ public class FormularioGeneral2 extends AppCompatActivity {
     CheckBox chkUnam;
     CheckBox chkIpn;
     CheckBox chkUam;
+    CheckBox chkEscombros;
+    CheckBox chkAcordonar;
+    CheckBox chkRevisionEstructuras;
+    CheckBox chkRedElectrica;
+    CheckBox chkCarpinteria;
+    CheckBox chkSustanciasQuimicas;
+    CheckBox chkConstruccion;
+    CheckBox chkPrimerosAuxilios;
+    CheckBox chkZona1Lider; //modificar por menús desplegables
+    CheckBox chkZona2Lider;//modificar por menús desplegables
+    CheckBox chkZona3Lider;//modificar por menús desplegables
+    CheckBox chkZona1;//modificar por menús desplegables
+    CheckBox chkZona2;//modificar por menús desplegables
+    CheckBox chkZona3;//modificar por menús desplegables
+    CheckBox chkLugar1;//modificar por menús desplegables
+    CheckBox chkLugar2;//modificar por menús desplegables
+    CheckBox chkLugar3;//modificar por menús desplegables
+    CheckBox chkAgregarLugar; //modificar por submenú
+    CheckBox chkMixto;
+    CheckBox chkHombre;
+    CheckBox chkMujer;
+    CheckBox chkNinos;
+    CheckBox chkMascotas;
+    CheckBox chkBotas;
+    CheckBox chkChaleco;
+    CheckBox chkCasco;
+    CheckBox chkGuantes;
+    CheckBox chkCubrebocas;
+    CheckBox chkLentes;
+    CheckBox chkLunes;
+    CheckBox chkMartes;
+    CheckBox chkMiercoles;
+    CheckBox chkJueves;
+    CheckBox chkViernes;
+    CheckBox chkSabado;
+    CheckBox chkDomingo;
+    CheckBox chk0206;
+    CheckBox chk0610;
+    CheckBox chk1014;
+    CheckBox chk1418;
+    CheckBox chk1822;
+    CheckBox chk2202;
+
+
 
 
     @Override
@@ -95,6 +147,7 @@ public class FormularioGeneral2 extends AppCompatActivity {
 
         btnRegresar = (Button) findViewById(R.id.btn_Regresar);
         btnEnviarRegistro=(Button)findViewById(R.id.btn_Enviar_Registro);
+        btnIngresa=(Button)findViewById(R.id.btn_IngresarMiembro);
 
 
         lytSeccionWeb=(LinearLayout)findViewById(R.id.Seccion_Web);
@@ -112,7 +165,11 @@ public class FormularioGeneral2 extends AppCompatActivity {
         txtTitulo=(TextView)findViewById(R.id.txt_TituloActividad);
         txtDescripcion=(TextView)findViewById(R.id.txt_DescripcionActividad);
 
-
+        // COMPLETADO Sección de checkbox
+        chkFacebook=(CheckBox)findViewById(R.id.chk_Facebook);
+        chkTwitter=(CheckBox)findViewById(R.id.chk_Twitter);
+        chkInstagram=(CheckBox)findViewById(R.id.chk_Instagram);
+        chkSnapchat=(CheckBox)findViewById(R.id.chk_Snapchat);
         chkProteccion=(CheckBox)findViewById(R.id.chk_Proteccion);
         chkSismologico=(CheckBox)findViewById(R.id.chk_Sismologico);
         chkMeteorologico=(CheckBox)findViewById(R.id.chk_Meteorologico);
@@ -126,6 +183,50 @@ public class FormularioGeneral2 extends AppCompatActivity {
         chkUnam=(CheckBox)findViewById(R.id.chk_UNAM);
         chkIpn=(CheckBox)findViewById(R.id.chk_IPN);
         chkUam=(CheckBox)findViewById(R.id.chk_UAM);
+        chkEscombros=(CheckBox)findViewById(R.id.chk_Recoger_escombros);
+        chkAcordonar=(CheckBox)findViewById(R.id.chk_Acordonar_area);
+        chkRevisionEstructuras=(CheckBox)findViewById(R.id.chk_Revision_estructuras);
+        chkRedElectrica=(CheckBox)findViewById(R.id.chk_Red_electrica);
+        chkCarpinteria=(CheckBox)findViewById(R.id.chk_Carpinteria);
+        chkSustanciasQuimicas=(CheckBox)findViewById(R.id.chk_Sustancias_quimicas);
+        chkConstruccion=(CheckBox)findViewById(R.id.chk_Construccion);
+        chkPrimerosAuxilios=(CheckBox)findViewById(R.id.chk_Primeros_auxilios);
+        chkZona1Lider=(CheckBox)findViewById(R.id.chk_Zona1Lider);
+        chkZona2Lider=(CheckBox)findViewById(R.id.chk_Zona2Lider);
+        chkZona3Lider=(CheckBox)findViewById(R.id.chk_Zona3Lider);
+        chkZona1=(CheckBox)findViewById(R.id.chk_Zona1); //modificar para solo poder elegir una
+        chkZona2=(CheckBox)findViewById(R.id.chk_Zona2);//modificar para solo poder elegir una
+        chkZona3=(CheckBox)findViewById(R.id.chk_Zona3);//modificar para solo poder elegir una
+        chkLugar1=(CheckBox)findViewById(R.id.chk_Lugar1);
+        chkLugar2=(CheckBox)findViewById(R.id.chk_Lugar2);
+        chkLugar3=(CheckBox)findViewById(R.id.chk_Lugar3);
+        chkAgregarLugar=(CheckBox)findViewById(R.id.chk_Agregar_Lugar);
+        chkMixto=(CheckBox)findViewById(R.id.chk_ResidentesMixtoAlbergue);
+        chkHombre=(CheckBox)findViewById(R.id.chk_ResidentesHombresAlbergue);
+        chkMujer=(CheckBox)findViewById(R.id.chk_ResidentesMujeresAlbergue);
+        chkNinos=(CheckBox)findViewById(R.id.chk_ResidentesNinosAlbergue);
+        chkMascotas=(CheckBox)findViewById(R.id.chk_ResidentesMascotasAlbergue);
+        chkBotas=(CheckBox)findViewById(R.id.chk_Botas);
+        chkChaleco=(CheckBox)findViewById(R.id.chk_Chaleco);
+        chkCasco=(CheckBox)findViewById(R.id.chk_Casco);
+        chkGuantes=(CheckBox)findViewById(R.id.chk_Guantes);
+        chkCubrebocas=(CheckBox)findViewById(R.id.chk_Cubrebocas);
+        chkLentes=(CheckBox)findViewById(R.id.chk_Lentes);
+        chkLunes=(CheckBox)findViewById(R.id.chk_Lunes);
+        chkMartes=(CheckBox)findViewById(R.id.chk_Martes);
+        chkMiercoles=(CheckBox)findViewById(R.id.chk_Miercoles);
+        chkJueves=(CheckBox)findViewById(R.id.chk_Jueves);
+        chkViernes=(CheckBox)findViewById(R.id.chk_Viernes);
+        chkSabado=(CheckBox)findViewById(R.id.chk_Sabado);
+        chkDomingo=(CheckBox)findViewById(R.id.chk_Domingo);
+        chk0206=(CheckBox)findViewById(R.id.chk_0206);
+        chk0610=(CheckBox)findViewById(R.id.chk_0610);
+        chk1014=(CheckBox)findViewById(R.id.chk_1014);
+        chk1418=(CheckBox)findViewById(R.id.chk_1418);
+        chk1822=(CheckBox)findViewById(R.id.chk_1822);
+        chk2202=(CheckBox)findViewById(R.id.chk_2202);
+
+
 
     }
 
@@ -194,7 +295,7 @@ public class FormularioGeneral2 extends AppCompatActivity {
             startActivity(regresar);
         }
     }
-
+//¿QUÉ INSTIRUCIÓN TE GUSTARÍA CUBRIR?
     private void Unchecked(){
         chkProteccion.setChecked(false);
         chkSismologico.setChecked(false);
@@ -210,6 +311,8 @@ public class FormularioGeneral2 extends AppCompatActivity {
         chkIpn.setChecked(false);
         chkUam.setChecked(false);
     }
+    //¿?
+
 
     public void Proteccion(View CheckBox){
         Unchecked();
